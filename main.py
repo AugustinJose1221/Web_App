9916f4fcc25b73f9ad4c073a4997d5226aa1e67f
AugustinJose1221 <44978968+AugustinJose1221@users.noreply.github.com>
2019-06-28 12:34:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,62 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START gae_python37_render_template]
-import datetime
-
-from flask import Flask, render_template
-
+'''
+import csv
+import googlemaps
+import numpy as np
+'''
+from flask import Flask, flash, redirect, render_template,request,session,abort
+'''
+gmaps = googlemaps.Client(key='AIzaSyAMp71qLXdZl5x80f0hjazPeUmtUyWAOIw')        #API Key
+data = "example1.csv"                                        #Dataset
+h_list={}
+x=0
+with open(data, 'r+') as file:
+    reader = csv.reader(file)
+    hospital_list = list(reader)
+for a in hospital_list:
+    if a[1][0]=='N':
+        #print("\n")
+        continue
+    h_list[x,0]=a[4]
+    h_list[x,1]=a[3]
+    h_list[x,2]=a[1]
+    h_list[x,3]=a[2]
+    h_list[x,5]=a[0]
+    x+=1
+dist=25
+pos = 0
+'''
 app = Flask(__name__)
 
+@app.route("/")
+def my_form():
+    return render_template('test.html')
 
-@app.route('/')
-def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
-                   datetime.datetime(2018, 1, 2, 10, 30, 0),
-                   datetime.datetime(2018, 1, 3, 11, 0, 0),
-                   ]
+@app.route("/",methods=['POST'])
+def my_form_post():
+    '''
+    dist=25
+    lat = request.form['text1']
+    long = request.form['text2']
+    LatP = float(lat)
+    LongP = float(long)
+    for i in range(0,x):
+        t = float(h_list[i,2])-float(LatP)
+        p = float(h_list[i,3])-float(LongP)
+        if t<0.1 and t>-0.1:
+            if p<0.1 and p>-0.1:
+                LatD = h_list[i,2]
+                LongD = h_list[i,3]
+                distance = gmaps.distance_matrix([str(LatP) + " " + str(LongP)], [str(LatD) + " " + str(LongD)], mode='driving')['rows'][0]['elements'][0]
+                h_list[i,4]=distance["distance"]["text"]
+                h_list[i,7]=distance["distance"]["value"]/1000
+                if (distance["distance"]["value"]/1000)<dist:
+                    dist=distance["distance"]["value"]/1000
+                    pos=i
+                #print("Name :",h_list[i,0],"\nAddress :",h_list[i,1],"\nDistance :",h_list[i,4],"\n")
+    '''
+    nearest_park = "Django's garage" #str(h_list[pos,0])
+    greet = "Nearest parking lot is "+nearest_park
+    return render_template("test.html",name=greet)#processed_text
 
-    return render_template('test.html', times=dummy_times)
-
-
-if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
+if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True)
-# [START gae_python37_render_template]
