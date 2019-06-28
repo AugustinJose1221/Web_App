d8c2da08f215db2cc550881d31e31b10615b849a
AugustinJose1221 <44978968+AugustinJose1221@users.noreply.github.com>
2019-06-28 11:25:25 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -54,5 +54,5 @@ def my_form_post():
     return render_template("test.html",name=greet)#processed_text
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='127.0.0.1', port=8080, debug=True)
 
